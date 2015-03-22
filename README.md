c5c77b950bbd692ff0808389eb852d34018c21fc
Zeroware <zero@darksense.com>
2015-03-22 17:47:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Strike Search API client
+
+This is a simple node.js API client for the _Strike Search_ website (http://getstrike.net)
+
+## Quickstart
+
+```js
+	var strike = require('strike-api');
+	
+	strike.info('B425907E5755031BDA4A8D1B6DCCACA97DA14C04');
+```
+
+## Licence
+
+May be freely distributed under the MIT license.
+
+Copyright (c) 2015 zeroware
---
+++ test/strike.js
@@ -0,0 +1,71 @@
+var assert = require("assert");
+var strike = require('../lib/strike');
+
+describe('strike', function(){
+    describe('#info()', function(){
+        it('should return info on specified torrent hash', function(done) {
+            strike.info('B425907E5755031BDA4A8D1B6DCCACA97DA14C04').then(function(result) {
+
+                var status = result[0];
+                var data = result[1];
+
+                assert.equal(200, status.statuscode);
+                assert.equal(1, data.length);
+
+                assert.equal('B425907E5755031BDA4A8D1B6DCCACA97DA14C04', data[0].torrent_hash);
+
+                done();
+            }).catch(function(err) {
+                done(err);
+            });
+        })
+    });
+
+    describe('#downloadLink()', function(){
+        it('should return the download link for specified hash', function(done) {
+            strike.downloadLink('B425907E5755031BDA4A8D1B6DCCACA97DA14C04').then(function(result) {
+
+                assert.equal(200, result.statuscode);
+                assert.equal(true, result.message != null);
+
+                done();
+            }).catch(function(err) {
+                done(err);
+            });
+        })
+    });
+
+    describe('#search()', function(){
+        it('should return search results', function(done) {
+            strike.search('Slackware').then(function(result) {
+
+                var status = result[0];
+                var data = result[1];
+
+                assert.equal(200, status.statuscode);
+                assert.equal(true, status.results > 0);
+
+                assert.equal(true, Array.isArray(data));
+                assert.equal(true, data[0].torrent_hash !== null);
+
+                done();
+            }).catch(function(err) {
+                done(err);
+            });
+        })
+    });
+
+    describe('#countTotal()', function(){
+        it('should return total torrent count', function(done) {
+            strike.countTotal().then(function(result) {
+
+                assert.equal(200, result.statuscode);
+                assert.equal(true, result.indexed_torrents > 0);
+
+                done();
+            }).catch(function(err) {
+                done(err);
+            });
+        })
+    })
+});
